b5faed37bec7a14b8a7eade2838dec4860af704a
jarmytage <jasminegray_96@hotmail.co.uk>
2017-03-12 12:01:59 +0000

    initial cimmit

---
+++ TripBooking/src/Interfaces/Booking.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaces;
+
+import tripbooking.Classes.Student;
+
+/**
+ *
+ * @author jasmi
+ */
+public interface Booking {
+    
+    public Student student;
+}
